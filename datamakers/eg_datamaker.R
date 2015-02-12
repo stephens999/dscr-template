ac68094a3adb0f137b742985b7d0b897268acc39
stephens999 <stephens999@gmail.com>
2015-02-12 08:57:10 -0600

    Update eg_datamaker.R
    change it so seed set only if supplied for compatibility with dsc2BE

--- datamakers/eg_datamaker.R
+++ datamakers/eg_datamaker.R
@@ -2,9 +2,9 @@
 #each datamaker should take input seed (integer) and args (list), and output a list with names elements meta and input
 #the format of the meta and input elements should be defined in the README
 #
-eg.datamaker = function(seed,args){
+eg.datamaker = function(seed=NULL,args){
 
-  set.seed(seed)
+  if(!is.null(seed)){set.seed(seed)}
   
   #here insert the meat of the function that needs to be defined for each dsc to be done
   #Your function should define the variables meta (a list) and input (a list)
